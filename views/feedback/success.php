292b8c9dd78b8e94845da18cac473a2bcf5f5b18
avemorana <ana.shurkina@gmail.com>
2016-12-25 20:05:46 +0200

    Update success.php

--- views/feedback/success.php
+++ views/feedback/success.php
@@ -3,11 +3,11 @@
 <head>
     <?php
     $title = "Дякую";
-    require_once '/views/blocks/head.php';
+    require_once 'views/blocks/head.php';
     ?>
 </head>
 <body>
-<? require_once '/views/blocks/header.php' ?>
+<? require_once 'views/blocks/header.php' ?>
 <div class="content">
     <div class="alert alert-success" role="alert">
         <h1 class="titlePage display-4 alert-heading">Дякуємо!</h1>
@@ -15,6 +15,6 @@
         <h4 class="display-5">Ваше повідомлення успішно відправлене та буде розглянуте найближчим часом.</h4>
     </div>
 </div>
-<? require_once '/views/blocks/footer.php' ?>
+<? require_once 'views/blocks/footer.php' ?>
 </body>
 </html>
